7bc972dd057d35a5d3deb15aa86f4dd5023b5a38
ashfullstack <akashramsonule@gmail.com>
2025-02-23 01:41:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -287,7 +287,7 @@ app.get("/:shortUrl", (req, res) => {
 
 
 //const baseUrl = "https://shorturl-b9s8.onrender.com"; // Base URL for short links
-const baseUrl = "https://www.urlshortner.sbs"; // Base URL for short links
+const baseUrl = "urlshortner"; // Base URL for short links
 
 // app.post("/shorten", (req, res) => {
 //   const originalUrl = req.body.originalUrl;
